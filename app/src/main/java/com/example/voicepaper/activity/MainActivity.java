e1cfd2de4c27a57d5a070833bd23562947fc7b02
Moraeso <45632773+Moraeso@users.noreply.github.com>
2019-10-15 03:11:45 +0900

    Grid view bundle 사용 실패

--- app/src/main/java/com/example/voicepaper/activity/MainActivity.java
+++ app/src/main/java/com/example/voicepaper/activity/MainActivity.java
@@ -4,16 +4,33 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.voicepaper.R;
+import com.example.voicepaper.fragment.main.RoomGridFragment;
 import com.example.voicepaper.manager.AppManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment roomGridFragment;
+    FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         AppManager.getInstance().setContext(this);
+        AppManager.getInstance().setResources(getResources());
+
         setContentView(R.layout.activity_main);
+
+        roomGridFragment = new RoomGridFragment();
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace( R.id.fragment_roomGrid, roomGridFragment );
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/voicepaper/data/Room.java
+++ app/src/main/java/com/example/voicepaper/data/Room.java
@@ -1,4 +1,72 @@
 package com.example.voicepaper.data;
 
+import android.graphics.Bitmap;
+
 public class Room {
+    // Room(id, name, profileString, comment, hostID, [RoomMember<List>])
+
+    private int id;
+    private String name;
+    private Bitmap profileImage;
+    private String profileString;
+    private String comment;
+    private int hostID;
+    // private ArrayList<RoomMember> roomMemberItems;
+
+    public Room(int id, String name, Bitmap profileImage, String profileString, String comment, int hostID) {
+        this.id = id;
+        this.name = name;
+        this.profileImage = profileImage;
+        this.profileString = profileString;
+        this.comment = comment;
+        this.hostID = hostID;
+    }
+
+    public Bitmap getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(Bitmap profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileString() {
+        return profileString;
+    }
+
+    public void setProfileString(String profileString) {
+        this.profileString = profileString;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getHostID() {
+        return hostID;
+    }
+
+    public void setHostID(int hostID) {
+        this.hostID = hostID;
+    }
 }
