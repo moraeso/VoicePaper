56e633cf93de0998a931a045589773d03f79cf1f
SHINMH <sky95012@gmail.com>
2019-12-14 01:21:18 +0900

    포트 변경 수정

--- app/src/main/java/com/example/voicepaper/network/UploadFile.java
+++ app/src/main/java/com/example/voicepaper/network/UploadFile.java
@@ -46,7 +46,7 @@ public class UploadFile extends AsyncTask<Void, Void, Void> {
             Log.d("smh:userId",this.userId);
         }
         else if(type == UPLOAD_IMAGE_ROOM){
-            this.m_url = Constants.URL+"/file/uploadgroupimage";
+            this.m_url = Constants.URL+"/file/uploadroomimage";
             this.roomId = values.get("roomId").toString();
         }
         else if(type == UPLOAD_AUDIO){
